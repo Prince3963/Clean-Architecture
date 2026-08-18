c2a6a0b453614fe71fded608e2c2657788c8ebfe
Prince3963 <princepatel3963@gmail.com>
2026-08-18 12:33:03 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Application.DTOs;
+using MyApp.Application.Services.Interface;
+
+namespace MyApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService employeeService;
+        private readonly IValidator<AddEmployeeDTO> validator;
+        public EmployeeController(IEmployeeService employeeService, IValidator<AddEmployeeDTO> validator)
+        {
+            this.employeeService = employeeService;
+            this.validator = validator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee()
+        {
+            var employees = await employeeService.GetEmployee();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] AddEmployeeDTO employee)
+        {
+            var validationResult = await validator.ValidateAsync(employee);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+            var newEmployee = await employeeService.AddEmployee(employee);
+            return Ok(newEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var deletedEmployee = await employeeService.DeleteEmployee(id);
+            return Ok(deletedEmployee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, AddEmployeeDTO employee)
+        {
+            var updatedEmployee = await employeeService.UpdateEmployee(employee, id);
+            return Ok(updatedEmployee);
+        }
+    }
+}
---
+++ DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Domain;
+using MyApp.Infrastcture;
+
+namespace MyApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAPIDI(this IServiceCollection services)
+        {
+            services.AddInfrastrctureDI()
+            .AddApplicationDI()
+            .AddDomainDI();
+            return services;
+        }
+    }
+}
